1285c9fab275a0f5cbf34570339360f5d8a2b4e9
ratul-space <to.ratul.hosen@gmail.com>
2024-01-19 22:43:42 +0600

    feat: update WordDetailActivity

--- app/src/main/java/com/example/simpledictionary/WordDetailActivity.kt
+++ app/src/main/java/com/example/simpledictionary/WordDetailActivity.kt
@@ -2,6 +2,7 @@ package com.example.simpledictionary
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.TextView
 
 class WordDetailActivity : AppCompatActivity() {
     companion object{
@@ -11,5 +12,21 @@ class WordDetailActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_word_detail)
+
+        val wordId = intent.getStringExtra(WORD_ID) ?: ""
+        if (wordId.isBlank()){
+            finish()
+        }
+        val dbHelper = DataBaseHelper(applicationContext)
+        val cursor = dbHelper.getWord(wordId)
+        if (cursor.moveToFirst()){
+            val textWord = findViewById<TextView>(R.id.txtWord)
+            val textType = findViewById<TextView>(R.id.txtType)
+            val textMeaning = findViewById<TextView>(R.id.txtMeaning)
+
+            textWord?.text = cursor.getString(0)
+            textType?.text = cursor.getString(1)
+            textMeaning?.text = cursor.getString(2)
+        }
     }
 }

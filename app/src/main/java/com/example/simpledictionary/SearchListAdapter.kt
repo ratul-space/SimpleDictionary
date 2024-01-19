574254e31fbedc671af6b81de976176d6b9e840f
ratul-space <to.ratul.hosen@gmail.com>
2024-01-19 22:43:01 +0600

    feat: update SearchListAdapter

--- app/src/main/java/com/example/simpledictionary/SearchListAdapter.kt
+++ app/src/main/java/com/example/simpledictionary/SearchListAdapter.kt
@@ -2,6 +2,7 @@ package com.example.simpledictionary
 
 import android.content.Context
 import android.database.Cursor
+import android.graphics.Color
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
@@ -9,6 +10,11 @@ import android.widget.CursorAdapter
 import android.widget.TextView
 
 class SearchListAdapter(context: Context, cursor: Cursor):CursorAdapter(context, cursor, 0) {
+    private class viewHolder{
+        val textWord: TextView? = null
+        val textType: TextView? = null
+        val textMeaning: TextView? = null
+    }
     override fun newView(context: Context?, cursor: Cursor?, parent: ViewGroup?): View {
        val layoutInflater = LayoutInflater.from(context)
         val newView = layoutInflater.inflate(R.layout.search_list_item, parent, false)
@@ -23,5 +29,15 @@ class SearchListAdapter(context: Context, cursor: Cursor):CursorAdapter(context,
      textWord?.text = cursor?.getString(0)
      textType?.text = cursor?.getString(1)
      textMeaning?.text = cursor?.getString(2)
+    }
+
+    override fun getView(position: Int, convertView: View?, parent: ViewGroup?): View {
+       val view =  super.getView(position, convertView, parent)
+        if (position % 2 == 0){
+            view.setBackgroundColor(Color.LTGRAY)
+        }else{
+            view.setBackgroundColor(Color.WHITE)
+        }
+        return view
     }
 }
